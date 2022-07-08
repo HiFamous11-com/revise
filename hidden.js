cdca56f1509c96a38534db6d0098c80c9502ff5f
HiFamous11-com <80319540+HiFamous11-com@users.noreply.github.com>
2022-07-08 12:09:26 +0530

    again

--- hidden.js
+++ hidden.js
@@ -5,4 +5,25 @@ function btn() {
     } else {
       x.style.display = "block";
     }
+    var x = document.getElementById("unhidden");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
   }
+
+  function btns() {
+    var x = document.getElementById("unhidden");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    var x = document.getElementById("hidden");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }  
